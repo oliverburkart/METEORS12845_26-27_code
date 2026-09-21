552fcfd91408c458361df8a844de74814ea2a4d2
Oliver <oliverhburkart@gmail.com>
2026-09-21 13:58:02 -0400

    Add intake support

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/Control.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/Control.java
@@ -1,5 +1,37 @@
 // This class is for the motors and servos not directly related to driving
 package org.firstinspires.ftc.teamcode.core;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class Control {
+
+    private CRServo leftintake;
+    private CRServo rightintake;
+    private DcMotor topintake;
+
+    public Control(CRServo leftintake, CRServo rightintake, DcMotor topintake) {
+        this.leftintake = leftintake;
+        this.rightintake = rightintake;
+        this.topintake = topintake;
+    }
+
+    public void intakeIn(double power) {
+        leftintake.setPower(-power);
+        rightintake.setPower(power);
+        topintake.setPower(power);
+    }
+
+    public void intakeOut(double power) {
+        leftintake.setPower(power);
+        rightintake.setPower(-power);
+        topintake.setPower(-power);
+    }
+
+    public void stop() {
+        leftintake.setPower(0);
+        rightintake.setPower(0);
+        topintake.setPower(0);
+    }
 }
